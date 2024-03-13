8e73eac5e669e9eb1305e5e79bf3b77899a0337a
VascoPanigi <156548239+VascoPanigi@users.noreply.github.com>
2024-03-13 16:22:53 +0100

    prove

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const newTask = document.getElementById("todo-add");
 const submitButton = document.getElementById("submit-btn");
 const taskContainer = document.getElementById("myUl");
 
+//aggiungere task - metodo giusto? boh...
+
 const handleSubmit = () => {
   if (newTask.value === "") {
     alert("Fill the form please");
@@ -9,5 +11,13 @@ const handleSubmit = () => {
     let newLiItem = document.createElement("li");
     newLiItem.innerText = newTask.value;
     taskContainer.appendChild(newLiItem);
+
+    let deleteBin = document.createElement("span");
+    deleteBin.innerText = "X";
+    newLiItem.appendChild(deleteBin);
   }
+  newTask.value = "";
 };
+
+//eliminare le task
+taskContainer.addEventListener("click", (event) => {});
